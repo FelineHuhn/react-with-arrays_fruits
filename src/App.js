dc48f44420d21bd0952a16b9d9509c84913cfc43
Feline Huhn <feline.huhn@gmx.de>
2024-02-07 11:20:21 +0100

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "🍌 Banana",
+      color: "LemonChiffon",
+    },
+    {
+      id: 2,
+      name: "🍓 Strawberry",
+      color: "MistyRose",
+    },
+    {
+      id: 3,
+      name: "🥝 Kiwi",
+      color: "Khaki",
+    },
+    {
+      id: 4,
+      name: "🍍 Pineapple",
+      color: "AntiqueWhite",
+    },
+    {
+      id: 5,
+      name: "🍑 Peach",
+      color: "PeachPuff",
+    },
+    {
+      id: 6,
+      name: "🫐 Blueberry",
+      color: "Lavender",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
